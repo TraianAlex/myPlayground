1d78281a48a523ae680d4f28affa361ee03e4156
Traian Alexandru <victor_traian@yahoo.com>
2020-12-29 17:09:02 -0500

    refactor

--- src/Hooks/KeyProp/Contact.js
+++ src/Hooks/KeyProp/Contact.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect, useReducer } from "react";
 
 const defaultValuesByTopic = {
-  training: 'I would like some training',
-  consulting: 'I have consulting needs',
-  question: 'I have some questions',
-}
+  training: "I would like some training",
+  consulting: "I have consulting needs",
+  question: "I have some questions",
+};
 
 const Contact = () => {
-  const [topic, setTopic] = React.useState('training')
+  const [topic, setTopic] = useState("training");
   return (
     <form>
       <label htmlFor="topic">Topic</label>
-      <select id="topic" value={topic} onChange={e => setTopic(e.target.value)}>
+      <select
+        id="topic"
+        value={topic}
+        onChange={(e) => setTopic(e.target.value)}
+      >
         <option value="training">Training</option>
         <option value="consulting">Consulting</option>
         <option value="question">Question</option>
@@ -25,38 +29,38 @@ const Contact = () => {
       <label htmlFor="body">Email body</label>
       <textarea id="body" />
     </form>
-  )
-}
+  );
+};
 
 const Counter = () => {
-  console.log('Counter called')
+  console.log("Counter called");
 
-  const [count, setCount] = React.useState(() => {
-    console.log('Counter useState initializer')
-    return 0
-  })
-  const increment = () => setCount(c => c + 1)
+  const [count, setCount] = useState(() => {
+    console.log("Counter useState initializer");
+    return 0;
+  });
+  const increment = () => setCount((c) => c + 1);
 
-  React.useEffect(() => {
-    console.log('Counter useEffect callback')
+  useEffect(() => {
+    console.log("Counter useEffect callback");
     return () => {
-      console.log('Counter useEffect cleanup')
-    }
-  }, [])
+      console.log("Counter useEffect cleanup");
+    };
+  }, []);
 
-  console.log('Counter returning react elements')
-  return <button onClick={increment}>{count}</button>
-}
+  console.log("Counter returning react elements");
+  return <button onClick={increment}>{count}</button>;
+};
 
 const CounterParent = () => {
-  const [keyCounterKey, setKeyCounterKey] = React.useReducer(c => c + 1, 0)
+  const [keyCounterKey, setKeyCounterKey] = useReducer((c) => c + 1, 0);
   return (
     <div>
       <button onClick={setKeyCounterKey}>reset</button>
       <Counter key={keyCounterKey} />
     </div>
-  )
-}
+  );
+};
 
 export const AppContact = () => {
   return (
@@ -65,5 +69,5 @@ export const AppContact = () => {
       <hr />
       <CounterParent />
     </div>
-  )
-}
+  );
+};
