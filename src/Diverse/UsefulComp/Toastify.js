aa5c2e17468b1f96fb58c46b6eb763a7dec50629
Traian Alexandru <victor_traian@yahoo.com>
2021-06-03 12:31:08 -0400

    fix toastify

--- src/Diverse/UsefulComp/Toastify.js
+++ src/Diverse/UsefulComp/Toastify.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // minified version is also included
 // import 'react-toastify/dist/ReactToastify.min.css';
 
 export class Toastify extends Component {
-  notify = () => toast("Wow so easy notifications!");
+  notify = () => toast('Wow so easy notifications!');
 
   render() {
     return (
@@ -23,9 +23,26 @@ export class Toastify extends Component {
 toast.configure();
 
 export const Toastify2 = () => {
-  const notify = () => toast("Wow so easy !");
+  const notify2 = () => toast('Wow so easy2 !');
 
-  return <button onClick={notify}>Notify !</button>;
+  return (
+    <>
+      <button onClick={notify2}>Notify2 !</button>
+      <ToastContainer />
+    </>
+  );
+};
+
+export const Toast = () => {
+  const notify = () => toast('Wow so easy!');
+
+  return (
+    <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+    </div>
+  );
 };
 
 // https://github.com/fkhadra/react-toastify
+// https://fkhadra.github.io/react-toastify/introduction/
